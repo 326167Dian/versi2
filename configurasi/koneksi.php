ad0910a34862a296caee67ef7d16347f46c4f041
Ahmad Nurdiansyah <ahmadnurdiansyah81@gmail.com>
2025-11-05 01:33:31 +0000

    perbaikan koneksi ke database

--- configurasi/koneksi.php
+++ configurasi/koneksi.php
@@ -1,25 +1,25 @@
 <?php
-// Date_Default_timezone_set('Asia/jakarta');
-// $server = "localhost";
-//  $user = "u877780297_apotekbarokah";
-//  $password = "7390091979Dian&&";
-//  $database = "u877780297_barokah";
-//  set_time_limit(1800);
-
-// ($GLOBALS["___mysqli_ston"] = mysqli_connect($server, $user, $password)) or die("Koneksi gagal");
-// mysqli_select_db($GLOBALS["___mysqli_ston"], $database) or die("Database tidak ditemukan");
-// // $koneksi = new mysqli($server, $user, $password) or die("Koneksi gagal");
-// $db = mysqli_connect('localhost', 'u877780297_apotekbarokah', '7390091979Dian&&', 'u877780297_barokah');
-
 Date_Default_timezone_set('Asia/jakarta');
 $server = "localhost";
-$user = "root";
-$password = "";
-$database = "enggal";
-set_time_limit(1800);
+ $user = "u725913413_best";
+ $password = "7390091979Dian&&";
+ $database = "u725913413_mysifabest";
+ set_time_limit(1800);
 
 ($GLOBALS["___mysqli_ston"] = mysqli_connect($server, $user, $password)) or die("Koneksi gagal");
 mysqli_select_db($GLOBALS["___mysqli_ston"], $database) or die("Database tidak ditemukan");
 // $koneksi = new mysqli($server, $user, $password) or die("Koneksi gagal");
-$db = mysqli_connect('localhost', 'root', '', 'enggal'); 
+$db = mysqli_connect('localhost', 'u725913413_best', '7390091979Dian&&', 'u725913413_mysifabest');
+
+// Date_Default_timezone_set('Asia/jakarta');
+// $server = "localhost";
+// $user = "root";
+// $password = "";
+// $database = "enggal";
+// set_time_limit(1800);
+
+// ($GLOBALS["___mysqli_ston"] = mysqli_connect($server, $user, $password)) or die("Koneksi gagal");
+// mysqli_select_db($GLOBALS["___mysqli_ston"], $database) or die("Database tidak ditemukan");
+// // $koneksi = new mysqli($server, $user, $password) or die("Koneksi gagal");
+// $db = mysqli_connect('localhost', 'root', '', 'enggal'); 
 ?>
