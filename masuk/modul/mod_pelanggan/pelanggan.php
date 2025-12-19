99ff6cbefc66a54568e663e610ed4490d859c420
326167Dian <ahmadnurdiansyah81@gmail.com>
2025-12-20 06:49:06 +0700

    perubahan ke server side

--- masuk/modul/mod_pelanggan/pelanggan.php
+++ masuk/modul/mod_pelanggan/pelanggan.php
@@ -16,96 +16,78 @@ if (empty($_SESSION['username']) and empty($_SESSION['passuser'])) {
 ?>
 
 
-<div class='box box-primary box-solid'>
-    <div class='box-header with-border'>
-        <h3 class='box-title'>DATA PELANGGAN</h3>
-        <div class='box-tools pull-right'>
-            <button class='btn btn-box-tool' data-widget='collapse'><i class='fa fa-minus'></i></button>
-        </div><!-- /.box-tools -->
-    </div>
-    <div class='box-body table-responsive'>
-        <table id='tampil' class='table table-bordered table-striped'>
-            <thead>
-                <tr>
-                    <th>No</th>
-                    <th>Nama Pelanggan</th>
-                    <th>Telepon</th>
-                    <th>Alamat</th>
-                    <th>Keterangan</th>
-                    <th width='120'>Aksi</th>
-                </tr>
-            </thead>
-        </table>
-    </div>
-</div>
+			<div class="box box-primary box-solid">
+				<div class="box-header with-border">
+					<h3 class="box-title">DATA PELANGGAN</h3>
+					<div class="box-tools pull-right">
+						<button class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
+					</div><!-- /.box-tools -->
+				</div>
+				<div class="box-body table-responsive">
+					<a class='btn  btn-success btn-flat' href='?module=pelanggan&act=tambah'>TAMBAH</a>
+					<br><br>
+
+
+					<table id="tampil" class="table table-bordered table-striped">
+						<thead>
+							<tr>
+								<th>No</th>
+								<th>Nama Pelanggan</th>
+								<th>Telepon</th>
+								<th>Alamat</th>
+								<th>Keterangan</th>
+								<th width="70">Aksi</th>
+							</tr>
+						</thead>
+						<tbody>
+				</tbody></table>
+<!-- DataTables server-side: rows are loaded via ajax -->
+
+				<script>
+					$(document).ready(function() {
+						$("#tampil").DataTable({
+							processing: true,
+							serverSide: true,
+							autoWidth: false,
+							ajax: {
+								"url": "modul/mod_pelanggan/pelanggan_serverside.php?action=table_data",
+								"dataType": "JSON",
+								"type": "POST"
+							},
+							columns: [{
+								"data": "no",
+								"className": 'text-center',
+							},
+							{
+								"data": "nm_pelanggan"
+							},
+							{
+								"data": "tlp_pelanggan"
+							},
+							{
+								"data": "alamat_pelanggan"
+							},
+							{
+								"data": "ket_pelanggan"
+							},
+							{
+								"data": "pilih",
+								"className": 'text-center'
+							}
+						]
+						});
+					});
+				</script>
+	
+				</div>
+			</div>
 
 
 <?php
 
 			break;
 
-            $pel = mysqli_query($GLOBALS["___mysqli_ston"], "SELECT * FROM pelanggan WHERE id_pelanggan='$_GET[id]'");
-            $pp = mysqli_fetch_array($pel);
-
-            echo "
-          <div class='box box-primary box-solid'>
-                <div class='box-header with-border'>
-                    <h3 class='box-title'>TAMBAH RIWAYAT - $pp[nm_pelanggan]</h3>
-                    <div class='box-tools pull-right'>
-                        <button class='btn btn-box-tool' data-widget='collapse'><i class='fa fa-minus'></i></button>
-                    </div><!-- /.box-tools -->
-                </div>
-                <div class='box-body table-responsive'>
-
-                        <form method=POST action='$aksi?module=pelanggan&act=input_riwayat' class='form-horizontal'>
-
-                              <input type=hidden name='id_pelanggan' value='$pp[id_pelanggan]'>
-
-                              <div class='form-group'>
-                                    <label class='col-sm-2 control-label'>Tgl</label>                
-                                     <div class='col-sm-3'>
-                                        <input type=date name='tgl' class='form-control' value='".date('Y-m-d')."' required>
-                                     </div>
-                              </div>
-
-                              <div class='form-group'>
-                                    <label class='col-sm-2 control-label'>Diagnosa</label>                
-                                     <div class='col-sm-6'>
-                                        <textarea name='diagnosa' class='form-control' rows='3' required></textarea>
-                                     </div>
-                              </div>
-
-                              <div class='form-group'>
-                                    <label class='col-sm-2 control-label'>Tindakan</label>                
-                                     <div class='col-sm-6'>
-                                        <textarea name='tindakan' class='form-control' rows='3' required></textarea>
-                                     </div>
-                              </div>
-
-                              <div class='form-group'>
-                                    <label class='col-sm-2 control-label'>Follow Up</label>                
-                                     <div class='col-sm-6'>
-                                        <textarea name='followup' class='form-control' rows='2'></textarea>
-                                     </div>
-                              </div>
-
-                              <div class='form-group'>
-                                    <label class='col-sm-2 control-label'></label>       
-                                        <div class='col-sm-5'>
-                                            <input class='btn btn-info' type=submit value=SIMPAN>
-                                            <input class='btn btn-primary' type=button value=BATAL onclick=self.history.back()>
-                                        </div>
-                                </div>
-                                
-                              </form>
-                              
-                </div> 
-                
-            </div>";
-
-
-            break;
-        case "riwayat":
+		case "tambah":
 
 			echo "
 		  <div class='box box-primary box-solid'>
@@ -158,10 +140,11 @@ if (empty($_SESSION['username']) and empty($_SESSION['passuser'])) {
 							  </form>
 							  
 				</div> 
-				</div>
+				
 			</div>";
 
 
+			break;
 
 		case "edit":
 			$edit = mysqli_query($GLOBALS["___mysqli_ston"], "SELECT * FROM pelanggan WHERE id_pelanggan='$_GET[id]'");
@@ -228,30 +211,3 @@ if (empty($_SESSION['username']) and empty($_SESSION['passuser'])) {
 	}
 }
 ?>
-
-<script>
-	$(document).ready(function() {
-		$('#tampil').DataTable({
-			processing: true,
-			serverSide: true,
-			ajax: {
-				"url": "modul/mod_pelanggan/pelanggan_serverside.php?action=table_data",
-				"dataType": "JSON",
-				"type": "POST"
-			},
-			columns: [
-				{ "data": "no", "className": 'text-center' },
-				{ "data": "nm_pelanggan" },
-				{ "data": "tlp_pelanggan" },
-				{ "data": "alamat_pelanggan" },
-				{ "data": "ket_pelanggan" },
-				{ "data": "aksi", "visible": <?= ($_SESSION['level'] == 'pemilik') ? 'true' : 'false'; ?>, "render": function(data, type, row){
-					var btn = "<div style='text-align:center'><a href='?module=pelanggan&act=edit&id="+data+"' title='EDIT' class='btn btn-warning btn-xs'>EDIT</a> "+
-					"<a href='?module=pelanggan&act=riwayat&id="+data+"' title='Riwayat' class='btn btn-info btn-xs'>RIWAYAT</a> "+
-					"<a href=javascript:confirmdelete('modul/mod_pelanggan/aksi_pelanggan.php?module=pelanggan&act=hapus&id="+data+"') title='HAPUS' class='btn btn-danger btn-xs'>HAPUS</a></div>";
-					return btn;
-				} }
-			]
-		});
-	});
-</script>
--- masuk/modul/mod_pelanggan/pelanggan_serverside.php
+++ masuk/modul/mod_pelanggan/pelanggan_serverside.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 include_once '../../../configurasi/koneksi.php';
 
 if ($_GET['action'] == "table_data") {
@@ -9,33 +10,45 @@ if ($_GET['action'] == "table_data") {
         2 => 'tlp_pelanggan',
         3 => 'alamat_pelanggan',
         4 => 'ket_pelanggan',
-        5 => 'id_pelanggan'
+        5 => 'id_pelanggan',
     );
+
+    $aksi = "modul/mod_pelanggan/aksi_pelanggan.php";
 
     $querycount = $db->query("SELECT count(id_pelanggan) as jumlah FROM pelanggan");
     $datacount = $querycount->fetch_array();
 
     $totalData = $datacount['jumlah'];
-
     $totalFiltered = $totalData;
 
-    $limit = $_POST['length'];
-    $start = $_POST['start'];
-    $order = $columns[$_POST['order']['0']['column']];
-    $dir = $_POST['order']['0']['dir'];
+    $limit = intval($_POST['length']);
+    $start = intval($_POST['start']);
+    $colIndex = intval($_POST['order']['0']['column']);
+    $order = isset($columns[$colIndex]) ? $columns[$colIndex] : 'id_pelanggan';
+    $dir = (isset($_POST['order']['0']['dir']) && strtolower($_POST['order']['0']['dir']) === 'asc') ? 'ASC' : 'DESC';
 
     if (empty($_POST['search']['value'])) {
-        $query = $db->query("SELECT id_pelanggan, nm_pelanggan, tlp_pelanggan, alamat_pelanggan, ket_pelanggan FROM pelanggan ORDER BY $order $dir LIMIT $limit OFFSET $start");
+        $query = $db->query("SELECT * FROM pelanggan ORDER BY $order $dir LIMIT $limit OFFSET $start");
     } else {
         $search = $db->real_escape_string($_POST['search']['value']);
-        $query = $db->query("SELECT id_pelanggan, nm_pelanggan, tlp_pelanggan, alamat_pelanggan, ket_pelanggan FROM pelanggan WHERE nm_pelanggan LIKE '%$search%' OR tlp_pelanggan LIKE '%$search%' OR alamat_pelanggan LIKE '%$search%' OR ket_pelanggan LIKE '%$search%' ORDER BY $order $dir LIMIT $limit OFFSET $start");
+        $query = $db->query("SELECT * FROM pelanggan 
+            WHERE nm_pelanggan LIKE '%$search%' 
+            OR tlp_pelanggan LIKE '%$search%' 
+            OR alamat_pelanggan LIKE '%$search%' 
+            OR ket_pelanggan LIKE '%$search%' 
+            ORDER BY $order $dir LIMIT $limit OFFSET $start");
 
-        $querycount = $db->query("SELECT count(id_pelanggan) as jumlah FROM pelanggan WHERE nm_pelanggan LIKE '%$search%' OR tlp_pelanggan LIKE '%$search%' OR alamat_pelanggan LIKE '%$search%' OR ket_pelanggan LIKE '%$search%'");
+        $querycount = $db->query("SELECT count(id_pelanggan) as jumlah FROM pelanggan 
+            WHERE nm_pelanggan LIKE '%$search%' 
+            OR tlp_pelanggan LIKE '%$search%' 
+            OR alamat_pelanggan LIKE '%$search%' 
+            OR ket_pelanggan LIKE '%$search%'");
         $datacount = $querycount->fetch_array();
         $totalFiltered = $datacount['jumlah'];
     }
 
     $data = array();
+
     if (!empty($query)) {
         $no = $start + 1;
         while ($value = $query->fetch_array()) {
@@ -44,7 +57,27 @@ if ($_GET['action'] == "table_data") {
             $nestedData['tlp_pelanggan'] = $value['tlp_pelanggan'];
             $nestedData['alamat_pelanggan'] = $value['alamat_pelanggan'];
             $nestedData['ket_pelanggan'] = $value['ket_pelanggan'];
-            $nestedData['aksi'] = $value['id_pelanggan'];
+
+            if(
+                isset($_SESSION['level']) && $_SESSION['level'] == 'pemilik'
+            ){
+                $nestedData['pilih'] = '<div class="dropdown">'
+                    . '<button class="btn btn-default dropdown-toggle" type="button" data-toggle="dropdown">Action <i class="fa fa-caret-down"></i></button>'
+                    . '<div class="dropdown-menu">'
+                    . '<a href="?module=pelanggan&act=edit&id=' . $value['id_pelanggan'] . '" title="EDIT" class="btn btn-info btn-xs" style="width:100%; margin:5px 0">EDIT</a>'
+                    . '<a href="?module=pelanggan&act=riwayat&id=' . $value['id_pelanggan'] . '" title="RIWAYAT" class="btn btn-success btn-xs" style="width:100%; margin:5px 0">RIWAYAT</a>'
+                    . '<a href="javascript:confirmdelete(\'' . $aksi . '?module=pelanggan&act=hapus&id=' . $value['id_pelanggan'] . '\')" title="HAPUS" class="btn btn-danger btn-xs" style="width:100%; margin:5px 0">HAPUS</a>'
+                    . '</div>'
+                    . '</div>';
+            } else {
+                $nestedData['pilih'] = '<div class="dropdown">'
+                    . '<button class="btn btn-default dropdown-toggle" type="button" data-toggle="dropdown">Action <i class="fa fa-caret-down"></i></button>'
+                    . '<div class="dropdown-menu">'
+                    . '<a href="?module=pelanggan&act=edit&id=' . $value['id_pelanggan'] . '" title="EDIT" class="btn btn-info btn-xs" style="width:100%; margin:5px 0">EDIT</a>'
+                    . '<a href="?module=pelanggan&act=riwayat&id=' . $value['id_pelanggan'] . '" title="RIWAYAT" class="btn btn-success btn-xs" style="width:100%; margin:5px 0">RIWAYAT</a>'
+                    . '</div>'
+                    . '</div>';
+            }
 
             $data[] = $nestedData;
             $no++;
@@ -52,10 +85,10 @@ if ($_GET['action'] == "table_data") {
     }
 
     $json_data = [
-        "draw"            => intval($_POST['draw']),
-        "recordsTotal"    => intval($totalData),
-        "recordsFiltered" => intval($totalFiltered),
-        "data"            => $data
+        "draw"              => intval($_POST['draw']),
+        "recordsTotal"      => intval($totalData),
+        "recordsFiltered"   => intval($totalFiltered),
+        "data"              => $data
     ];
 
     echo json_encode($json_data);
